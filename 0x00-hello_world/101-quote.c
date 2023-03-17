b605de55ca1dcf9fe36454123870cdcd7440c433
root <sokayna.bennasseur@gmail.com>
2023-03-16 19:47:07 -0700

    quote

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,18 @@
+#include <stdio.h> 
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char string[]="and that piece of art is useful\"\ \-\ Dora Korpar, 2015\-10\-19";
+	int i=0;
+
+	while(string[i] != '\0') {
+	       putchar(string[i]);
+       	       i++;
+	}
+	return (1);	
+}
