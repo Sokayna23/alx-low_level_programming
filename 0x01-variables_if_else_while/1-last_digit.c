f59db98cd6fb7808c624f6b9c02c4f30bd3d0f73
root <sokayna.bennasseur@gmail.com>
2023-03-17 17:01:24 -0700

    last degit

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -12,18 +12,18 @@
 int main(void)
 {
 	int n;
-	int last_degit;
+	int last;
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	/* your code goes there */
-	last_degit = n % 10;
+	last = n % 10;
 
-	if (last_degit > 5)
-		printf("Last degit of %d is %d and is greater than 5\n", n, last_degit);
-	else if (last_degit == 0)
-		printf("Last degit of %d is %d and is 0\n", n, last_degit);
-	else if (last_degit < 6)
-		printf("Last degit of %d is %d and is less than 6 and not 0\n", n, last_degit);
+	if (last > 5)
+		printf("Last degit of %d is %d and is greater than 5\n", n, last);
+	else if (last == 0)
+		printf("Last degit of %d is %d and is 0\n", n, last);
+	else if (last < 6)
+		printf("Last degit of %d is %d and is less than 6 and not 0\n", n, last);
 	return (0);
 }
