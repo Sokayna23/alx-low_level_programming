9cc165138661de4653de576f5c41f706fa8479cd
root <sokayna.bennasseur@gmail.com>
2023-03-17 17:06:12 -0700

    last degit

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -23,7 +23,7 @@ int main(void)
 		printf("Last degit of %d is %d and is greater than 5\n", n, last);
 	else if (last == 0)
 		printf("Last degit of %d is %d and is 0\n", n, last);
-	else if (last < 6)
+	else if (last < 6 && != 0)
 		printf("Last degit of %d is %d and is less than 6 and not 0\n", n, last);
 	return (0);
 }
