039c1cd2611ecb1030b1ae65bf83fea210acf916
root <sokayna.bennasseur@gmail.com>
2023-03-17 19:05:41 -0700

    lowerUPPER

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -10,12 +10,16 @@ int main(void)
 	char lower_letter = 'a';
 	char upper_letter = 'A';
 
-	while (lower_letter <= 'z' && upper_letter <= 'Z')
+	while (lower_letter <= 'z')
 	{
 		putchar(lower_letter);
 		lower_letter++;
+	}
+	while (upper_letter <= 'Z')
+	{
 		putchar(upper_letter);
 		upper_letter++;
 	}
+	putchar('\n');
 	return (0);
 }
