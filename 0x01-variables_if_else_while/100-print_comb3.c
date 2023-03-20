3135ea5ba59de45fb650e16f232201157352021f
root <sokayna.bennasseur@gmail.com>
2023-03-20 11:04:25 -0700

    combination 3" git push

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int x, y;
+
+	for (x = 48; x < 57; x++)
+	{
+		for (y = 49; y < 58; y++)
+		{
+			if (y > x)
+			{
+				putchar(x);
+				putchar(y);
+				if (x != y)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
