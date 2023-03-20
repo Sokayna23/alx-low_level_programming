a45f7d9799e87c3c56957b46078d020e30e88887
root <sokayna.bennasseur@gmail.com>
2023-03-20 09:32:55 -0700

    Combination

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -9,13 +9,13 @@ int main(void)
 {
 	int n;
 
-	for (n = 0; n < 10; n++)
+	for (n = 48; n < 58; n++)
 	{
 		putchar(n);
 		putchar(',');
 		putchar(' ');
-		if (n == 9)
-			putchar('$');
+		if (n == 57)
+			putchar('\n');
 	}
 	return (0);
 }
