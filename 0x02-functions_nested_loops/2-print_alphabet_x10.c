c46cd84e9440e18bc064a18bc6d74ddcca66314b
root <sokayna.bennasseur@gmail.com>
2023-03-21 18:57:03 -0700

    print x10

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -7,17 +7,18 @@
  */
 void print_alphabet_x10(void)
 {
-	char c = 'a';
+	char c;
 	int i = 0;
 
 	while (i < 10)
 	{
+		c = 'a';
 		while (c <= 'z')
 		{
 			_putchar(c);
 			c++;
 		}
+		_putchar('\n');
 		i++;
 	}
-	_putchar('\n');
 }
