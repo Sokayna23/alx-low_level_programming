d06afdd07d8b8064e4f44099e8bc7ca63730da3c
root <sokayna.bennasseur@gmail.com>
2023-03-29 12:21:26 -0700

    Concatenates two strings

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -12,11 +12,12 @@ char *_strcat(char *dest, char *src)
 	int j;
 
 	for (i = 0; dest[i] != '\0'; i++);
+	i--;
 	for (j = 0; src[j] != '\0'; j++)
 	{
 		dest[i] = src[j];
 		i++;
 	}
-	*dest = '\0';
+	dest[i] = '\0';
 	return(dest);
 }
