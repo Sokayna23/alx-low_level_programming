36a7e4f47b7d382df9528a7b8b60475b1329801b
root <sokayna.bennasseur@gmail.com>
2023-03-29 23:54:42 -0700

    strcmp

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -10,12 +10,9 @@ int _strcmp(char *s1, char *s2)
 {
 	int i;
 
-	while (s1[i] != '\0' && s2[i] != '\0')
+	while (s1[i] != '\0' && s2[i] != '\0' && s1[i] == s2[i])
 	{
 		i++;
 	}
-	if (s1[i] == '\0' && s2[i] == '\0')
-		return (0);
-	else
-		return (s1[i] - s2[i]);
+	return (s1[i] - s2[i]);
 }
