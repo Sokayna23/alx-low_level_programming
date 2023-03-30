7cf7224c3530878d49ad547161d0391efc7706fc
root <sokayna.bennasseur@gmail.com>
2023-03-29 22:10:10 -0700

    strncat

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -9,18 +9,20 @@
  */
 char *_strncat(char *dest, char *src, int n)
 {
-	int i = 0;
-	int L = 0;
+	int i =0;
+	int j;
 
-	while (dest[L] != '\0')
+	while (dest[i] != '\0')
 	{
-		L++;
+		i++;
 	}
-	while (i < n && src[i] != '\0')
+	j = 0;
+	while (j < n && src[j] != '\0')
 	{
-		dest[L + i] = src[i];
-		i++
+		dest[i] = src[j];
+		i++;
+		j++;
 	}
-	dest[L + i] = '\0';
+	dest[i] = '\0';
 	return (dest);
 }
