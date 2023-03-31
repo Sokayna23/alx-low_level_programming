a2848e70850192a38b8c8ec8f7b7934b7f61cefd
root <sokayna.bennasseur@gmail.com>
2023-03-30 19:08:03 -0700

    rot13

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,29 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * rot13 - encodes rot13
+ * @s: pointer to string
+ * Return: a pointer to a string
+ */
+
+char *rot13(char *c)
+{
+	int i;
+	int j;
+	char abc[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+	char abcrot[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+
+	for (i = 0; c[i] != '\0'; i++)
+	{
+		for (j = 0; j < 52; j++)
+		{
+			if (c[i] == abc[j])
+			{
+				c[i] = abcrot[j];
+				break;
+			}
+		}
+	}
+	return (c);
+}
