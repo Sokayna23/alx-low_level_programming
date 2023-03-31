d193bddfaa291cc13c5b9292108a858199cb5309
root <sokayna.bennasseur@gmail.com>
2023-03-30 18:49:31 -0700

    leet

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -7,22 +7,23 @@
  */
 char *leet(char *str)
 {
-	int i;
+	int i, j;
 
-	for (i = 0; str[i] != '\0'; i++)
+	for (i = 0, j = 0; str[i] != '\0'; i++, j++)
 	{
-		if (str[i] == 'a' && str[i] == 'A')
-			str[i] = '4';
-		else if (str[i] == 'e' && str[i] == 'E')
-			str[i] = '3';
-		else if (str[i] == '0' && str[i] == 'O')
-			str[i] = '0';
-		else if (str[i] == 't' && str[i] == 'T')
-			str[i] = '7';
-		else if (str[i] == 'l' && str[i] == 'L')
-			str[i] = '1';
+		if (str[i] == 'a'|| str[i] == 'A')
+			str[j] = '4';
+		else if (str[i] == 'e' || str[i] == 'E')
+			str[j] = '3';
+		else if (str[i] == '0' || str[i] == 'O')
+			str[j] = '0';
+		else if (str[i] == 't' || str[i] == 'T')
+			str[j] = '7';
+		else if (str[i] == 'l' || str[i] == 'L')
+			str[j] = '1';
 		else
-			continue;
+			str[j] = str[i];
 	}
+	str[j] = '\0';
 	return (str);
 }
