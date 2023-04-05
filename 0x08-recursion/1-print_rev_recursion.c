555b48ff0202f26ddde1484e1d38c2059f9412be
root <sokayna.bennasseur@gmail.com>
2023-04-05 10:08:06 -0700

    reverse recursion

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -1,8 +1,16 @@
 #include "main.h"
+#include <string.h>
 
 /**
  * _print_rev_recursion - Prints strings in reverse.
  * @s: A pointer
  * Return: Nothing
  */
-void
+void _print_rev_recursion(char *s)
+{
+	if (*s)
+	{
+		_print_rev_recursion(s + 1);
+		_putchar(*s);
+	}
+}
