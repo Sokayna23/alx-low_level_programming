c2ba142f7f6e3741fcb5406377cba9e95613f2f5
root <sokayna.bennasseur@gmail.com>
2023-04-05 19:22:48 -0700

    square root

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,35 @@
+#include "main.h"
+
+/**
+ * _pow_recursion - returns the power of x to y
+ * @x: an integer
+ * @y: an integer
+ * Return: an integer
+ */
+int _pow_recursion(int x, int y)
+{
+if (y < 0)
+return (-1);
+if (y == 0)
+return (1);
+if (x == 0)
+return (0);
+else
+return (x * _pow_recursion(x, y - 1));
+}
+/**
+ * _sqrt_recursion - Returns the natural square root of a number
+ * @n: an integer
+ * Return: an integer
+ */
+int _sqrt_recursion(int n)
+{
+if (n < 0)
+return (-1);
+if (n == 0)
+return (0);
+if (n == 1)
+return (1);
+else
+return (_pow_recursion(n, 2));
+}
