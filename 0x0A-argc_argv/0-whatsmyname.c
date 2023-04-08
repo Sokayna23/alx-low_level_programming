dba93250eecc2d20cfd50689517b46b7377bef71
root <sokayna.bennasseur@gmail.com>
2023-04-08 12:10:28 -0700

    my name

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - Prints the name of this program
+ * @argc: an integer
+ * @argv: a pointer that points to an array of characters
+ * Return: 0.
+ */
+int main(int argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+	(void)argc;
+	return (0);
+}
