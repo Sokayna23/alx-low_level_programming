1a3727c9dc31fae7e9ba54afa6ef716baa79fd59
root <sokayna.bennasseur@gmail.com>
2023-04-09 11:36:00 -0700

    coins

---
+++ 0x0A-argc_argv/100-change.c
@@ -0,0 +1,10 @@
+ #include <stdio.h>
+
+/**
+ * main - Prints the minimum number of coins
+ * @argc: an integer
+ * @argv: a pointer to a character
+ * Return: Always 0.
+ */
+int main(int argc, char *argv[])
+
