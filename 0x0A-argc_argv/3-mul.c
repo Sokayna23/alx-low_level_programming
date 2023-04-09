bd1f449078019662134300209a154697b959e09e
root <sokayna.bennasseur@gmail.com>
2023-04-09 07:22:27 -0700

    multiplication

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -1,5 +1,40 @@
 #include <stdio.h>
-#include <stdlib.h>
+
+#include "main.h"
+
+/**
+ * _atoi - Converts a string to an integer.
+ * @s: The pointer to a character
+ * Return: An integer.
+ */
+int _atoi(char *s)
+{
+	int i = 0;
+	unsigned int integer = 0;
+	int sign = 1;
+	int is_digit = 0;
+
+	while (s[i] != '\0')
+	{
+		if (s[i] == 45)
+		{
+			sign = sign * (-1);
+		}
+		while (s[i] >= 48 && s[i] <= 57)
+		{
+			is_digit = 1;
+			integer = (integer * 10) + (s[i] - '0');
+			i++;
+		}
+		if (is_digit == 1)
+		{
+			break;
+		}
+		i++;
+	}
+	integer = integer * sign;
+	return (integer);
+}
 /**
  * main - Multiplies two numbers
  * @argc: an integer represents the size of the array argv[]
@@ -15,7 +50,7 @@ int main(int argc, char *argv[])
 		printf("Error\n");
 		return (1);
 	}
-	multp = atoi(argv[1]) * atoi(argv[2]);
+	multp = _atoi(argv[1]) * _atoi(argv[2]);
 	printf("%d\n", multp);
 	return (0);
 }
