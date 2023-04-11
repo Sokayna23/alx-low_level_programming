82124e8d8e2ed1589b904ac9f54548fd419c5176
root <sokayna.bennasseur@gmail.com>
2023-04-11 09:47:17 -0700

    create array

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -13,17 +13,17 @@ char *create_array(unsigned int size, char c)
 	char *array;
 	unsigned int i;
 
+	if (size == 0)
+	{
+		return (NULL);
+	}
 	array = malloc(size * (sizeof(char)));
 	if (array == NULL)
+	{
 		return (NULL);
-	i = 0;
+	}
 	for (i = 0; i < size; i++)
 	{
-		if (size == 0)
-		{
-			printf("failed to allocate memory\n");
-			return (NULL);
-		}
 		array[i] = c;
 	}
 	return (array);
