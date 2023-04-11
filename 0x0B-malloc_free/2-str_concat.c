49c2394ca3bcb64cf0f615bcaedd10c65b492eb4
root <sokayna.bennasseur@gmail.com>
2023-04-11 13:43:23 -0700

    concatinate two strings

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -21,8 +21,9 @@ char *str_concat(char *s1, char *s2)
 	for (j = 0; s2[j] != '\0'; j++)
 	{
 		s1[i] = s2[j];
-		s[i] = s1[i];
 		i++;
 	}
+	for (i = 0; s1[i] != '\0'; i++)
+		s[i] = s1[i];
 	return (s);
 }
