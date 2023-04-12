3a694f261f146ae6a2b6a41f14fa242827cca429
root <sokayna.bennasseur@gmail.com>
2023-04-11 19:30:59 -0700

    concatinates two strings

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -16,9 +16,17 @@ char *str_concat(char *s1, char *s2)
 	unsigned int l2 = strlen(s2);
 
 	if (s1 == NULL)
+	{
 		s1 = "";
+		return (s2);
+	}
 	if (s2 == NULL)
-		s2 = "";	
+	{
+		s2 = "";
+		return (s1);
+	}
+	if (s2 == NULL)
+		s2 = "";
 	s = malloc(sizeof(char) * (l1 + l2 + 1));
 	if (s == NULL)
 		return (NULL);
