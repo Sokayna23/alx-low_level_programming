6e1aac44cfdf763b5ce7927360524e4ce25e547e
root <sokayna.bennasseur@gmail.com>
2023-04-13 10:36:37 -0700

    nconcat

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -42,9 +42,10 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	{
 		p[i] = s1[i];
 	}
-	for (j = 0; j < n; j++)
+	for (j = 0; j < l2; j++)
 	{
 		p[i + j] = s2[j];
 	}
+	p[i + j] = '\0';
 	return (p);
 }
