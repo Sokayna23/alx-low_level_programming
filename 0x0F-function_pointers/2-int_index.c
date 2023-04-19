2e51ac3396845ead730e89d6302c203bfa659cfc
root <sokayna.bennasseur@gmail.com>
2023-04-19 05:52:49 -0700

    index

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -14,8 +14,8 @@ int int_index(int *array, int size, int (*cmp)(int))
 	if (size <= 0)
 		return (-1);
 	if (array == NULL || cmp == NULL)
-		return (0);
-	for (i = 0; i < size; i++)
+		return (-1);
+	for (i = 0; i < size && size > 0; i++)
 	{
 		if (cmp(array[i]))
 			return (i);
