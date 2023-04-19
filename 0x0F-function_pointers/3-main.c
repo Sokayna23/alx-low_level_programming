ab950bdb575e8de9aaed6b5db66c99081b08b890
root <sokayna.bennasseur@gmail.com>
2023-04-19 09:11:59 -0700

    op

---
+++ 0x0F-function_pointers/3-main.c
@@ -0,0 +1,36 @@
+#include "3-calc.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - checks the code
+ *@argc: arguments counter
+ *@argv: array
+ * Return: Always 0.
+ */
+int main(int argc, char *argv[])
+{
+	char *operator;
+	int num1, num2;
+
+	if (argc != 4)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+	num1 = atoi(argv[1]);
+	operator = argv[2];
+	num2 = atoi(argv[3]);
+	if (get_op_func(operator) == NULL)
+	{
+		printf("Error\n");
+		exit(99);
+	}
+	if ((*operator == '/' && num2 == 0) || (*operator == '%' && num2 == 0))
+	{
+		printf("Error\n");
+		exit(100);
+	}
+	printf("%d\n", get_op_func(operator)(num1, num2));
+	return (0);
+}
---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,52 @@
+#include "3-calc.h"
+
+/**
+ * op_add - adds two integers
+ * @a: number
+ * @b: number
+ * Return: the sum of a and b
+ */
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+/**
+ * op_sub - subtraction
+ * @a: number
+ * @b: number
+ * Return: the diferrence of a and b
+ */
+int op_sub(int a , int b)
+{
+	return (a - b);
+}
+/**
+ * op_mul - multiplication
+ * @a: number
+ * @b: number
+ * Return: the product of a and b
+ */
+int op_mult(int a, int b)
+{
+	return (a * b);
+}
+/**
+ * op_div - division
+ * @a: number
+ * @b: number
+ * Return: the result of the division of a by b
+ */
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+/**
+ * op_mod - modulo
+ * @a: number
+ * @b: number
+ * Return: the remainder of the division of a by b
+ */
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
