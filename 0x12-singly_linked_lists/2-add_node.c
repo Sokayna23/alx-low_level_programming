43128d2210891062937a9dab3ebf8c2db4fdf429
root <sokayna.bennasseur@gmail.com>
2023-04-27 11:50:24 -0700

    update

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -10,13 +10,13 @@
 list_t *add_node(list_t **head, const char *str)
 {
 	list_t *node;
-	
+
 	node = malloc(sizeof(list_t));
 	if (node == NULL)
 		return (NULL);
 	node->str = strdup(str);
 	node->len = strlen(str);
-	node->next = (*head);
-	(*head) = node;
+	node->next = *head;
+	*head = node;
 	return (node);
 }
