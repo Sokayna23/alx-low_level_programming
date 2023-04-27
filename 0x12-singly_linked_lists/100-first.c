cd741229ca7c5d4e8d80c398aaa56acec2ba7084
root <sokayna.bennasseur@gmail.com>
2023-04-27 14:08:22 -0700

    update

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,10 +1,10 @@
 #include "lists.h"
 
 /**
- * before_main - prints a string befor main
+ * print_msg - prints a string befor main
  * Return: nothing
  */
-void __attribute__((constructor))before_main()
+void __attribute__((constructor))print_msg()
 {
 char *c;
 c = "You're beat! and yet, you must allow,\nI bore my house upon my back!\n";
