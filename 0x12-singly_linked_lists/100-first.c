329806ddfb403a41df9d53938189bcc1b12c86fd
root <sokayna.bennasseur@gmail.com>
2023-04-27 13:21:03 -0700

    update

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,10 @@
+#include "lists.h"
+
+/**
+ * print_message - prints a string befor main
+ * Return: nothing
+ */
+void print_message(void)
+{
+	printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
+}
