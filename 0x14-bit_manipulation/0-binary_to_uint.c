1a7b32cadc8a31cb6e3b0e45b384554e01bba3b8
root <sokayna.bennasseur@gmail.com>
2023-05-04 12:26:03 -0700

    push

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -3,7 +3,7 @@
 
 /**
  * power_of_two - calculates the power of 2
- * @exponent: exponent
+ * @k: the exponent
  * Return: positive number;
  */
 unsigned int power_of_two(unsigned int k)
@@ -11,7 +11,7 @@ unsigned int power_of_two(unsigned int k)
 	if (k == 0)
 		return (1);
 	else
-		return (2 * power_of_two(k-1));
+		return (2 * power_of_two(k - 1));
 }
 /**
  * binary_to_uint - converts a binary number to an unsigned integer
@@ -20,19 +20,19 @@ unsigned int power_of_two(unsigned int k)
  */
 unsigned int binary_to_uint(const char *b)
 {
-	unsigned int i, len, k, n = 0;
+	unsigned int i, len, k, n;
 
 	if (!b)
 		return (0);
 	len = strlen(b);
-	for (i = 0; i < len; i++)
+	n = 0;
+	i = 0;
+	while (b[i] == 48 || b[i] == 49)
 	{
-		if (b[i] == '0' || b[i] == '1')
-		{
-			k = len - i - 1;
-			n = n + b[i] * power_of_two(k);
-			return (n);
-		}
+		k = len - i - 1;
+		n = n + b[i] * power_of_two(k);
+		i++;
+		return (n);
 	}
 	return (0);
 }
