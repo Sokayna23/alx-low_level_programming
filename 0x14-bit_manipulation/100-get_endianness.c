1db103f5418b9c1c4a4b2d6bb15bf3d8075bfbad
root <sokayna.bennasseur@gmail.com>
2023-05-04 18:40:17 -0700

    push

--- 0x14-bit_manipulation/100-get_endianness.c
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -1,6 +1,9 @@
 #include "main.h"
 
-
+/**
+ * get_endianness - checks the endianness
+ * Return: 1, if architecture is little endian, 0 in case of big endian.
+ */
 int get_endianness(void) {
     int n = 1;
     char *c = (char *) & n;
