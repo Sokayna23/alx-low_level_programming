5c385f183e2cfa8bbb16beca65259e89ed73f112
Sokayna <sokayna.bennasseur@gmail.com>
2023-05-09 08:07:35 -0700

    append

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -11,17 +11,20 @@ int append_text_to_file(const char *filename, char *text_content)
 	int file;
 	int txt;
 
-	file = open(filename, O_WRONLY | O_APPEND);
+	file = open(filename, O_WRONLY | O_APPEND, 0664);
 	if (!file)
-		return (-1);
-	txt = write(file, text_content, strlen(text_content));
-	if (!txt)
-		return (-1);
-	if (!text_content)
 	{
-		if (file)
-			return (1);
+		close(file);
 		return (-1);
+	}
+	if (text_content)
+	{
+		txt = write(file, text_content, strlen(text_content));
+		if (txt == -1)
+		{
+			close(file);
+			return (-1);
+		}
 	}
 	close(file);
 	return (1);
