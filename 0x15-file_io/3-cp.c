0fbeae6f86dfd26c708dbce3abffe3e6d382097d
Sokayna <sokayna.bennasseur@gmail.com>
2023-05-09 17:02:43 -0700

    copy

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,16 +1,72 @@
 #include "main.h"
 
 /**
- * close_err - handles closing errors
- * @fd: file descriptor
+ * open_file - opens files
+ * @file_from: first file
+ * @file_to: second file
+ * @fd1: file descriptor
+ * @fd2: file descreptor
  */
-void close_err(int fd)
+void open_files(const char *file_from, const char *file_to, int *fd1, int *fd2)
 {
-	if (close(fd) == -1)
+	*fd1 = open(file_from, O_RDONLY);
+	if (*fd1 == -1)
 	{
-		dprintf(2, "Error: Can't close fd %d\n", fd);
+		dprintf(STDERR_FILENO, "Error: Can't read from file %d\n", -1);
+		exit(98);
+	}
+	*fd2 = open(file_to, O_WRONLY | O_CREAT | O_TRUNC, 0664);
+	if (*fd2 == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't write to %d\n", -1);
+		exit(99);
+	}
+}
+/**
+ * close_files - close files descreptors and exits on error
+ * @fd1: the file descriptor of the file to close
+ * @fd2: the file descriptor of the file to close
+ */
+void close_files(int fd1, int fd2)
+{
+	if (close(fd1) == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", -1);
 		exit(100);
 	}
+	if (close(fd2 == -1))
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", -1);
+		exit(100);
+	}
+}
+/**
+ * cp_ - copies file_from to file_to
+ * @fd1: file descriptor of the file to copy from
+ * @fd2: fike descriptor of the file to copy to
+ */
+void cp_(int fd1, int fd2)
+{
+	ssize_t r, w;
+	char buffer[1024];
+
+	while ((r = read(fd1, buffer, 1024)) != 0)
+	{
+		if (r == -1)
+		{
+			dprintf(STDERR_FILENO, "Error: Can't read from file %d\n", fd1);
+			close(fd1);
+			exit(98);
+		}
+		w = write(fd2, buffer, r);
+		if (w == -1)
+		{
+			dprintf(STDERR_FILENO, "Error: Can't write to %d\n", fd2);
+			close(fd2);
+			exit(99);
+		}
+	}
+
 }
 /**
  * main - entry point
@@ -20,42 +76,16 @@ void close_err(int fd)
  */
 int main(int ac, char **av)
 {
-	int fd1, fd2, r, w;
+	int fd1 = -1, fd2 = -1;
 	const char *file_from = av[1], *file_to = av[2];
-	char buffer[1024];
 
 	if (ac != 3)
 	{
 		dprintf(2, "Usage: cp file_from file_to\n");
 		exit(97);
 	}
-	fd1 = open(file_from, O_RDONLY);
-	if (fd1 == -1)
-	{
-		dprintf(2, "Error: Can't read from file %s\n", file_from);
-		exit(98);
-	}
-	fd2 = open(file_to, O_WRONLY | O_CREAT | O_TRUNC, 0644);
-	if (fd2 == -1)
-	{
-		dprintf(2, "Error: Can't write to %s\n", file_to);
-		exit(99);
-	}
-	while ((r = read(fd1, buffer, 1024)) != 0)
-	{
-		if (r == -1)
-		{
-			dprintf(2, "Error: Can't read from file %s\n", file_from);
-			exit(98);
-		}
-		w = write(fd2, buffer, r);
-		if (w == -1)
-		{
-			dprintf(2, "Error: Can't write to %s\n", file_to);
-			exit(99);
-		}
-	}
-	close_err(fd1);
-	close_err(fd2);
+	open_files(file_from, file_to, &fd1, &fd2);
+	cp_(fd1, fd2);
+	close_files(fd1, fd2);
 	return (0);
 }
--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -10,5 +10,7 @@ int _putchar(char c);
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
-void close_err(int fd);
+void close_files(int fd1, int fd2);
+void open_files(const char *file_from, const char *file_to, int *fd1, int *fd2);
+void cp_(int fd1, int fd2);
 #endif
