d001b235afca50f72b984fba3e8ba5b98b2f93e1
Sokayna <sokayna.bennasseur@gmail.com>
2023-05-11 20:36:04 -0700

    fixed main funct

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,73 +1,53 @@
 #include "main.h"
 
 /**
- * open_file - opens files
- * @file_from: first file
- * @file_to: second file
- * @fd1: file descriptor
- * @fd2: file descreptor
- */
-void open_file(const char *file_from, const char *file_to, int *fd1, int *fd2)
-{
-	*fd1 = open(file_from, O_RDONLY);
-	if (*fd1 == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", file_from);
-		exit(98);
-	}
-	*fd2 = open(file_to, O_WRONLY | O_CREAT | O_TRUNC, 0664);
-	if (*fd2 == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", file_to);
-		close(*fd1);
-		exit(99);
-	}
-}
-/**
- * close_error - prints file descreptor close  error
+ * close_error - prints an error message if the file
+ * descriptor couldn't be closed
  * @fd: the file descriptor of the file to close
  */
 void close_error(int fd)
 {
-	if (close(fd) == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd);
-		exit(100);
-	}
+	dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd);
+	exit(100);
 }
 /**
  * main - copies the content of a file to another file
- * @ac: number of arguments
- * @av: the arguments passed to the program
- * Return: always 0;
+ * @ac: number of arguments passed to the program
+ * @av: array of the arguments
+ * Return: Always 0 (Success).
  */
-int main(int ac, char **av)
+int main(int ac, char *av[])
 {
-	ssize_t r, w;
+	int fd1, fd2, r, w;
 	char buffer[1024];
-	int fd1, fd2;
 
 	if (ac != 3)
 	{
-		dprintf(2, "Usage: cp file_from file_to\n");
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
 		exit(97);
 	}
-	open_file(av[1], av[2], &fd1, &fd2);
-	while ((r = read(fd1, buffer, 1024)) != 0)
-	{
-		if (r == -1)
+	fd1 = open(av[1], O_RDONLY);
+	r = read(fd1, buffer, 1024);
+	fd2 = open(av[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
+	do {
+		if (fd1 == 1 || r == -1)
 		{
-			dprintf(STDERR_FILENO, "Error: Can't read from %d\n", fd1);
+			dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", av[1]);
 			exit(98);
 		}
 		w = write(fd2, buffer, r);
-		if (w == -1)
+		if (fd2 == -1 || w == -1)
 		{
-			dprintf(STDERR_FILENO, "Error: Can't write to %d\n", fd2);
+
+			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", av[2]);
 			exit(99);
 		}
-	}
-	close_error(fd1);
-	close_error(fd2);
+		r = read(fd1, buffer, 1024);
+		fd2 = open(av[2], O_WRONLY | O_APPEND);
+	} while (r > 0);
+	if (close(fd1) == -1)
+		close_error(fd1);
+	if (close(fd2) == -1)
+		close_error(fd2);
 	return (0);
 }
