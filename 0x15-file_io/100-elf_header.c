c61d89e174ade810f414f7098f8a1c4446eff2b9
Sokayna <sokayna.bennasseur@gmail.com>
2023-05-12 17:34:01 -0700

    elf

---
+++ 0x15-file_io/100-elf_header.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * is_elf - checks if a file is an ELF file or not
+ * @str: a pointer of string
+ */
+void is_elf(unsigned char *str)
+{
+	int i;
+
+	for (i = 0; i < 4; i++)
+	{
+		if (str[i] !=x 
+	}
+}
