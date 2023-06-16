3d7d59e8dfa95f2949d855c4b44560b5fdd8c6d2
Sokayna <sokayna.bennasseur@gmail.com>
2023-06-16 01:30:56 +0000

    Task0: print list

---
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+/**
+ * print_dlistint - prints a doubly linked list
+ * @h: a pointer to the first node of the list
+ * Return: the number of nodes
+ */
+size_t print_dlistint(const dlistint_t *h)
+{
+	const dlistint_t *new;
+	int count_nodes = 0;
+	if (h == NULL)
+		return (0);
+	new = h;
+	while (new != NULL)
+	{
+		printf("%d\n", new->n);
+		new = new->next;
+		count_nodes += 1;
+	}
+	return (count_nodes);
+}
