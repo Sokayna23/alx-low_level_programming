2f03f4be6137b986e84ecfeee66b3c247b9d0d4d
Sokayna <sokayna.bennasseur@gmail.com>
2023-06-18 18:11:04 +0000

    Task6: sum listsum list

--- 0x17-doubly_linked_lists/6-sum_dlistint.c
+++ 0x17-doubly_linked_lists/6-sum_dlistint.c
@@ -1,1 +1,19 @@
 #include "lists.h"
+
+/**
+ * sum_slistint - computes the sum of all the data of a linked list
+ * @head: a pointer to the first node of the list
+ * Return: the sum of all data of the list
+ */
+int sum_dlistint(dlistint_t *head)
+{
+        int sum = 0;
+
+        while (head)
+	{
+		sum = sum + head->n;
+		head = head->next;
+        }
+        return (sum);
+}
+
