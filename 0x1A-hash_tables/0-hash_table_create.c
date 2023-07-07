841cba61fd5fc09d5c2f987349dcdf9dd1ef113e
Sokayna <sokayna.bennasseur@gmail.com>
2023-07-07 16:27:03 -0400

    Task2: key -> index

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -14,11 +14,10 @@ hash_table_t *hash_table_create(unsigned long int size)
 	hash_table = malloc(sizeof(hash_table_t));
 	if (!hash_table)
 	{
-
 		return (NULL);
 	}
 	hash_table->size = size;
-	hash_table->array = malloc(sizeof(hash_table_t *) * size);
+	hash_table->array = (hash_node_t **)malloc(sizeof(hash_node_t *) * size);
 	if (!hash_table->array)
 	{
 		free(hash_table);
